dc3e916ff92ade1039bb5c819da5cd73b8dd4bc7
Alexander Londoño Espejo <alexlondon07@gmail.com>
2019-05-05 20:33:55 -0500

    Optimizando app

--- 04-todo/config/yargs.js
+++ 04-todo/config/yargs.js
@@ -1,29 +1,25 @@
+const description =  {
+    demand: true,
+    alias: 'd',
+    desc: 'Description command to do'
+}
+
+const completed = {
+    default: true,
+    alias: 'c',
+    desc: ''
+}
+
 const argv = require('yargs')
         .command('create', 'Create element to do',{
-            description: {
-                demand: true,
-                alias: 'd',
-                desc: 'Description command to create element'
-            }
+            description
         })  
         .command('update', 'Update element to do',{
-            description: {
-                demand: true,
-                alias: 'd',
-                desc: 'Description command to update element'
-            },
-            completed: {
-                default: true,
-                alias: 'c',
-                desc: ''
-            }
+            description,
+            completed
         })
         .command('delete', 'Delete element to do',{
-            description: {
-                demand: true,
-                alias: 'd',
-                desc: 'Description command to delete element'
-            }
+            description
         })
         .help()
         .argv;
