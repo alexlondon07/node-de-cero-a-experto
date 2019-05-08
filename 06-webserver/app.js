ec6019b214570bdd1b778884ec2bb18675eb8b9f
Alexander Londoño Espejo <alexlondon07@gmail.com>
2019-05-08 17:38:12 -0500

    Introducción a Express

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -1,4 +1,4 @@
-const http = require('http');
+/*const http = require('http');
 
 http.createServer( (req, res) =>{
 
@@ -13,4 +13,19 @@ http.createServer( (req, res) =>{
 })
 .listen(8181);
 
-console.log('Listen to port 8181');
+console.log('Listen to port 8181');*/
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    let response = {
+        name: "Alexander",
+        age: 27,
+        url: req.url
+    }
+    res.send( response );
+});
+app.listen(8181, ()=>{
+    console.log('Listen to port 8181');
+});
