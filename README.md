d341ce2b11d818d2aa71a895589670c5d412d0ca
Alexander Londoño Espejo <alexlondon07@gmail.com>
2019-05-10 23:58:34 -0500

    Limpieza

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-de-cero-a-experto
+
+https://www.udemy.com/node-de-cero-a-experto/
--- app.js
+++
@@ -1,9 +0,0 @@
-const argv = require('yargs').argv;
-
-const { createFile } = require('./03/app');
-
-let argv2 = process.argv;
-
-
-console.log(argv);
-console.log(argv2);
